24946392d67940703beb2b1f7e3027f36e530bc7
FikretArslan <arslanfikret33@gmail.com>
2022-07-07 20:36:28 +0300

    07/07.2022

---
+++ src/main/java/com/greenrent/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.greenrent.security.service;
+
+import com.greenrent.domain.User;
+import com.greenrent.exception.message.ErrorMessage;
+import com.greenrent.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@AllArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       User user= userRepository.findByEmail(email).orElseThrow(()->new
+                UsernameNotFoundException(String.format(ErrorMessage.USER_NOT_FOUND_MESSAGE,null)));
+       return UserDetailsImpl.build(user);
+    }
+}
